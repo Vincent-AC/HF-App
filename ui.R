f9f6fea6885eb9e4c31c8a197027f3998c8d1c82
Vincent Aranzana-Climent <vincent@aracli.org>
2021-09-01 11:47:56 +0200

    feature: Add home page with minimal tutorial

--- ui.R
+++ ui.R
@@ -10,6 +10,7 @@ library(shiny)
 library(dplyr)
 library(shinydashboard)
 library(plotly)
+source("functions/uiHomeTab.R",encoding = "UTF-8")
 source("functions/uiTab1Comp.R")
 source("functions/uiTab2Comp.R")
 dashboardPage(
@@ -22,12 +23,17 @@ dashboardPage(
                                   style = "cursor: pointer;"),
                                 class = "dropdown")),
         dashboardSidebar(sidebarMenu(
+                menuItem("Start here !",
+                         tabName = "home"),
                 menuItem("1 compartment Hollow Fibre",
                          tabName = "1comp"),
                 menuItem("2 compartment Hollow Fibre",
                          tabName = "2comp")
         )),
         dashboardBody(tabItems(
+                tabItem(tabName = "home",
+                        home
+                ),
                 tabItem(tabName = "1comp",
                         tab1Comp
 ),
