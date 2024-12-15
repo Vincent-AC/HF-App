3e2cb648c8dbdc5085ae9298ed794b96140a7b53
Vincent Aranzana-Climent <vincent.aranzana.climent@univ-poitiers.fr>
2024-12-15 21:03:09 +0100

    dev: Add rsconnect files and temporarily disable tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,7 +6,7 @@
 # * https://r-pkgs.org/tests.html
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
-library(testthat)
-library(HFApp)
-
-test_check("HFApp")
+# library(testthat)
+# library(HFApp)
+#
+# test_check("HFApp")
